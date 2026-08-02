d1e7c42bc4eb03598d028f58c22bbf781786ac7a
Metetto <metesipahi@gmail.com>
2026-08-02 18:52:03 +0300

    One tiny change
    Forgot to wire the kararmaSinyali to gameLoop

--- 1_Main.v
+++ 1_Main.v
@@ -61,7 +61,7 @@ wire[29:0] p1Time, p2Time, p3Time, p4Time;
 wire[3:0] timedOut, falselyStarted, allNaughtyBois;
 wire[3:0] currentTurnNew;
 
-gameLoop theMainTroublemaker(clk, rst, confinish, displinish, calcinish, turnNo, currentTurn, playersIn, up, down, left, right, center, p1Time, p2Time, p3Time, p4Time, timedOut, falselyStarted, gameOver, turnOver, currentTurnNew);
+gameLoop theMainTroublemaker(clk, rst, confinish, displinish, calcinish, turnNo, currentTurn, playersIn, up, down, left, right, center, p1Time, p2Time, p3Time, p4Time, timedOut, falselyStarted, gameOver, turnOver, turnOffDisplay, currentTurnNew);
 assign currentTurn = currentTurnNew;
 
 
